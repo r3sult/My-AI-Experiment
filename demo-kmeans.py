0d2e0038071cf604b57db0d8532d405cfad660ab
ridwanbejo <ridwanbejo@gmail.com>
2014-07-04 08:25:52 +0700

    clustering belum beres. sekarang mencoba naive bayes

--- demo-kmeans.py
+++ demo-kmeans.py
@@ -8,6 +8,7 @@ buku        : Data Mining, Jiawei Han
 # library yang dibutuhkan untuk clustering k-means
 import math
 import collections
+import copy
 
 # contoh data yang akan dicluster
 hasil_test_fc = { 
@@ -51,11 +52,8 @@ def do_cluster(points, seeds):
         for seed in seeds:
             temp_distance = get_euclidean_distance(points[point], seeds[seed]['point'])
             epoch_list.update({seed : temp_distance})
-        
-        #print "epoch list : ", epoch_list
-        
+            
         temp_min = find_min_dict(epoch_list)
-        #print "min epoch : ", temp_min
         
         if temp_min == '1':
             temp_cluster['1'].append(points[point]) 
@@ -74,7 +72,6 @@ def generate_center_of_cluster(temp_cluster):
         temp_y = 0
         temp_len = len(temp_cluster[cluster])
         for point in temp_cluster[cluster]:
-            print point[0], ' --- ', point[1]
             temp_x += point[0]
             temp_y += point[1]
         temp_center = {'name':'c'+str(temp_index), 'point':(temp_x * 1.0 / temp_len,  temp_y * 1.0 / temp_len)}
@@ -84,40 +81,33 @@ def generate_center_of_cluster(temp_cluster):
     return temp_center_of_cluster
 
 def check_center_of_cluster_similarity(old_center_of_cluster, new_center_of_cluster):
-    print "bejo :", collections.OrderedDict(sorted(old_center_of_cluster.items()))
-    print "ridwan :", collections.OrderedDict(sorted(new_center_of_cluster.items()))
+    print "bejo :", old_center_of_cluster
+    print "ridwan :", new_center_of_cluster
+    
+    for old_center in old_center_of_cluster:
+        print old_center_of_cluster[old_center]['point']
+        print new_center_of_cluster[old_center]['point']
+        haula = cmp(old_center_of_cluster[old_center]['point'], new_center_of_cluster[old_center]['point'])
+        print "haula: ", haula
+        print "\n"
+    
     counter = 0
-    
-    """
-    for new_center in new_center_of_cluster:
-        #print new_center_of_cluster[new_center]['point']
-        #print old_center_of_cluster[new_center]['point']
-        print new_center_of_cluster[new_center]['point'] == old_center_of_cluster[new_center]['point']
-        if new_center_of_cluster[new_center]['point'] == old_center_of_cluster[new_center]['point']:
-            print "bejo"
-            counter += 1
-                    
-    print "counter : ", counter        
-    if counter < 2:
-        return -1
-    else:
-        return 1
-    """
-    
+   
     
 # bagian utama program
 old_center_of_cluster = seeds
 temp_cluster = do_cluster(hasil_test_fc, seeds)
-print "temp_cluster - 0 : ", temp_cluster
+
     
-for i in range(1, 11):
-    temp_center_of_cluster = generate_center_of_cluster(temp_cluster)
-    #print temp_center_of_cluster
+for i in range(1, 5):
+    temp_center_of_cluster = generate_center_of_cluster(temp_cluster) 
+    print temp_center_of_cluster
+    
     temp_check_coc = check_center_of_cluster_similarity(old_center_of_cluster, temp_center_of_cluster)
-    print "check coc : ", temp_check_coc
+    del old_center_of_cluster
+    
     old_center_of_cluster = temp_center_of_cluster
+    print old_center_of_cluster
+    
     temp_cluster = do_cluster(hasil_test_fc, temp_center_of_cluster)
-    print "temp_cluster - ",i," : ", temp_cluster
-    print "\n"
-    
-    
+    del temp_center_of_cluster
